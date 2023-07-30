750cd920134774fd40c5d9c6513252f65f0bd69f
rob <rob_abcba@yahoo.com>
2023-07-30 14:36:24 -0700

    Add comments.

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,13 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+// Middleware
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
 
+// Route
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}...`);
