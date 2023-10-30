811601094e94d9db545d18f63d5b409dbddc2666
rob <rob_abcba@yahoo.com>
2023-10-30 10:43:49 -0700

    Add sendEmail functionality to server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const sendEmail = require("./utils/sendEmail");
 
 const app = express();
 
@@ -13,6 +14,25 @@ app.use(cors());
 // Route
 app.get("/", (req, res) => {
   res.send("Home Page Yo");
+});
+
+app.post("/api/sendemail", async (req, res) => {
+  const { email } = req.body;
+  try {
+    const send_to = email;
+    const sent_from = process.env.EMAIL_USER;
+    const reply_to = email;
+    const subject = "Thank you message";
+    const message = `
+      <h3>Hello ZinoYo</h3>
+      <p>Thanks for your YouTube Tutorials</p>
+      <p>Regards...</p>
+    `;
+    await sendEmail(subject, message, send_to, sent_from, reply_to);
+    res.status(200).json({ success: true, message: "Email Sent" });
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 });
 
 const PORT = process.env.PORT || 5001;
